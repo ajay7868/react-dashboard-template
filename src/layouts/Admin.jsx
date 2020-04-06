73db67720d3340551a41a61bc4183ddddca29686
KUMAR Ajay <ajay.kumar5@soprasteria.com>
2020-04-06 16:44:26 +0530

    first commit

---
+++ src/layouts/Admin.jsx
@@ -0,0 +1,66 @@
+import React from "react";
+import PerfectScrollbar from "perfect-scrollbar";
+import { Route, Switch } from "react-router-dom";
+
+import DemoNavbar from "components/Navbars/DemoNavbar.jsx";
+
+import routes from "routes.js";
+
+var ps;
+
+class Dashboard extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      backgroundColor: "black",
+      activeColor: "info",
+    };
+    this.mainPanel = React.createRef();
+  }
+  componentDidMount() {
+    if (navigator.platform.indexOf("Win") > -1) {
+      ps = new PerfectScrollbar(this.mainPanel.current);
+      document.body.classList.toggle("perfect-scrollbar-on");
+    }
+  }
+  componentWillUnmount() {
+    if (navigator.platform.indexOf("Win") > -1) {
+      ps.destroy();
+      document.body.classList.toggle("perfect-scrollbar-on");
+    }
+  }
+  componentDidUpdate(e) {
+    if (e.history.action === "PUSH") {
+      this.mainPanel.current.scrollTop = 0;
+      document.scrollingElement.scrollTop = 0;
+    }
+  }
+  handleActiveClick = (color) => {
+    this.setState({ activeColor: color });
+  };
+  handleBgClick = (color) => {
+    this.setState({ backgroundColor: color });
+  };
+  render() {
+    return (
+      <div className="wrapper">
+        <div className="main-panel" ref={this.mainPanel}>
+          <DemoNavbar {...this.props} />
+          <Switch>
+            {routes.map((prop, key) => {
+              return (
+                <Route
+                  path={prop.layout + prop.path}
+                  component={prop.component}
+                  key={key}
+                />
+              );
+            })}
+          </Switch>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default Dashboard;
